1f19b30b30a18e7533358fa45be3a8616836ba31
Sage <sage.b.shaw@gmail.com>
2022-03-30 17:33:10 -0600

    finished adding suplemental tables

--- connections.py
+++ connections.py
@@ -1,3 +1,8 @@
+'''
+Tables from Su et al. Supplementary material.
+https://www.nature.com/articles/s41467-017-00191-6
+'''
+
 import numpy as np
 import pandas as pd
 
@@ -7,16 +12,19 @@ EBP_labels = columns = [f'EB-R{i}P' for i in range(8, 0, -1)] + [f'EB-L{i}P' for
 EIP_labels = [f'EIP{i}' for i in range(18)]
 PEI_labels = [f'PEI{i}' for i in range(16)]
 PEN_labels = [f'PEN{i}' for i in range(16)]
+tables = []
 
 EIP_PB = pd.DataFrame(
             data = 2*np.eye(18),
             columns = PB_labels, 
             index = EIP_labels)
+tables.append(EIP_PB)
 
 PEI_PB = pd.DataFrame(
             data = np.diag([1]*len(PB_labels))[1:-1],
             columns = PB_labels,
             index = PEI_labels)
+tables.append(PEI_PB)
 
 data = np.zeros((len(PEN_labels), len(PB_labels)))
 data[:8,:8] = np.diag([1]*8)
@@ -25,6 +33,7 @@ PEN_PB = pd.DataFrame(
             data = data,
             columns = PB_labels,
             index = PEN_labels)
+tables.append(PEN_PB)
 
 data = np.zeros((len(EIP_labels), len(EBC_labels)))
 data[0,0] = 1
@@ -43,6 +52,7 @@ EIP_EBC = pd.DataFrame(
             data = data,
             columns = EBC_labels,
             index = EIP_labels)
+tables.append(EIP_EBC)
 
 data = np.zeros((len(PEI_labels), len(EBC_labels)))
 for row in range(8):
@@ -55,32 +65,88 @@ PEI_EBC = pd.DataFrame(
             data = data,
             columns = EBC_labels,
             index = PEI_labels)
-
-PEN_EBC = pd.DataFrame(
-            data = np.zeros((len(PEN_labels), len(EBC_labels))),
-            columns = EBC_labels,
-            index = PEN_labels)
+tables.append(PEI_EBC)
 
 EIP_EBP = pd.DataFrame(
             data = EIP_EBC.to_numpy(),
             columns = EBP_labels,
             index = EIP_labels)
-
-PEI_EBP = pd.DataFrame(
-            data = np.zeros((len(PEI_labels), len(EBP_labels))),
-            columns = EBP_labels,
-            index = PEI_labels)
+tables.append(EIP_EBP)
 
 PEN_EBP = pd.DataFrame(
             data = PEI_EBC.to_numpy(),
             columns = EBP_labels,
             index = PEN_labels)
+tables.append(PEN_EBP)
 
+data = np.zeros((len(PEI_labels), len(EIP_labels)))
+for k in [-7, -8, 1, 9, 10]:
+    data += np.diag(np.ones(len(EIP_labels) - abs(k)), k = k)[:len(PEI_labels)]
+for row in range(8):
+    data[row, row + 2] = 1
+for row in range(8, 16):
+    data[row, row] = 1
+data[8, -2] = 1
+data[7, 1] = 1
 
+PEI_EIP = pd.DataFrame(
+            data = data,
+            columns = EIP_labels,
+            index = PEI_labels)
+tables.append(PEI_EIP)
 
+data = np.zeros((len(PEN_labels), len(EIP_labels)))
+for k in [-7, -8, 2, 10, 11]:
+    data += np.diag(np.ones(len(EIP_labels) - abs(k)), k = k)[:len(PEI_labels)]
+for row in range(8):
+    data[row, row + 3] = 1
+for row in range(8, 16):
+    data[row, row + 1] = 1
+data[8, -2] = 1
+data[7, 1] = 1
+PEN_EIP = pd.DataFrame(
+            data = data,
+            columns = EIP_labels,
+            index = PEN_labels)
+tables.append(PEN_EIP)
 
+# R_EIP to EIP
 
+data = np.zeros((len(EIP_labels), len(PEI_labels)))
+data[1:-1] = np.eye(16)
+EIP_PEI = pd.DataFrame(
+            data = data,
+            columns = PEI_labels,
+            index = EIP_labels)
+tables.append(EIP_PEI)
+
+# R_PEI to PEI
+
+data = np.zeros((len(EIP_labels), len(PEN_labels)))
+data[:8,:8] = np.eye(8)
+data[-8:,-8:] = np.eye(8)
+EIP_PEN = pd.DataFrame(
+            data = data,
+            columns = PEN_labels,
+            index = EIP_labels)
+tables.append(EIP_PEN)
+
+# R_PEN to PEN
+
+# tables on page 8+
 
 
 
 del(data)
+
+
+if __name__ == '__main__':
+    for tab in tables:
+        my_strs = tab.to_string().split('\n')
+        print('-'*len(my_strs[0]))
+        print(my_strs[0])
+        for s in my_strs[1:]:
+            print(s[:5], end='')
+            print(s[5:].replace('0', ' ').replace('.', ' '))
+        print('-'*len(my_strs[0]))
+
